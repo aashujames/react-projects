b7e6f62093377ecc7690f93008a4c53721369926
Ashish Gupta <ashishgupt25@gmail.com>
2023-06-10 16:20:23 +0530

    feat(SingleCocktail): get info of a particular cocktail

--- 15-cocktails/setup/src/components/Cocktail.js
+++ 15-cocktails/setup/src/components/Cocktail.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Cocktail = () => {
-  return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
-  )
-}
+const Cocktail = ({ item }) => {
+    const { image, name, id, info, glass } = item;
+    return (
+        <div className="cocktail">
+            <div className="img-container">
+                <img src={image} alt={name} />
+            </div>
+            <div className="cocktail-footer">
+                <h3>{name}</h3>
+                <h4>{glass}</h4>
+                <p>{info}</p>
+                <Link
+                    to={`/cocktail/${id}`}
+                    className="btn btn-primary btn-details"
+                >
+                    details
+                </Link>
+            </div>
+        </div>
+    );
+};
 
-export default Cocktail
+export default Cocktail;
--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
-  return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
-  )
-}
+    const { cocktails, loading } = useGlobalContext();
 
-export default CocktailList
+    // if (loading) {
+    //     return <Loading />;
+    // }
+
+    return (
+        <div className="section">
+            <h2 className="section-title">cocktails</h2>
+            <div className="cocktails-center">
+                {cocktails.map((item) => {
+                    return <Cocktail key={item.id} item={item} />;
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default CocktailList;
